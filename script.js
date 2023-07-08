8c6bb2a3aaed0b5caf39d090f24d82b745f91399
Manoj Poudel <februarymanoj2004@gmail.com>
2023-07-08 10:20:09 +0545

    just commited.

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  var menuBtn = document.querySelector(".menu-btn");
+
+  menu.classList.toggle("show-menu");
+  if (menu.classList.contains("show-menu")) {
+    menuBtn.style.display = "none";
+  } else {
+    menuBtn.style.display = "block";
+  }
+
+  var menuItems = document.querySelectorAll(".menu-btn");
+  menuItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      menu.classList.remove("show-menu");
+    });
+  });
+}
+function validateform() {
+  var name = document.forms["feedbackform"]["name"].value;
+  var email = document.forms["feedbackform"]["email"].value;
+  var phone = document.forms["feedbackform"]["phone"].value;
+  var message = document.forms["feedbackform"]["message"].value;
+  if (name == "" || email == "" || phone == "" || message == "") {
+    alert("please fill the form completely.");
+  } else {
+    alert("Thank you for your feedback.");
+  }
+}
